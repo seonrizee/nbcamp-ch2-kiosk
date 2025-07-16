023baa68fa8ac33cfd0aebb4451089b06fdb2c43
seonrizee <seonrizee@gmail.com>
2025-07-16 16:00:16 +0900

    feat(lv1): 키오스크 기본 구조 및 입력 루프 구현, 입력에 따른 로직 실행 함수 구현

--- src/main/java/io/github/seonrizee/kiosk/required/lv1/Main.java
+++ src/main/java/io/github/seonrizee/kiosk/required/lv1/Main.java
@@ -1,8 +1,34 @@
 package io.github.seonrizee.kiosk.required.lv1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Kiosk!");
+
+        try (Scanner sc = new Scanner(System.in)) {
+            boolean isRunning = true;
+            while (isRunning) {
+
+                System.out.println("롯데리아");
+                System.out.println("메뉴를 선택해주세요.");
+                System.out.println("0. 종료");
+
+                int cmd = Integer.parseInt(sc.nextLine());
+                if (cmd == 0) {
+                    System.out.println("키오스크를 종료합니다.");
+                    isRunning = false;
+                } else {
+                    operateMenu(cmd);
+                }
+            }
+
+        }
+
     }
+
+    public static void operateMenu(int cmd) {
+        System.out.println(cmd + "번 메뉴를 선택했습니다.");
+    }
+
 }
