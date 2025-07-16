789a9e81f99ec82491475a88fc8421df0988bbd2
seonrizee <seonrizee@gmail.com>
2025-07-16 16:36:46 +0900

    feat(lv1): 숫자가 아닌 형태에 대한 예외 처리, 출력 유틸리티 함수 추가 구현

--- src/main/java/io/github/seonrizee/kiosk/required/lv1/Main.java
+++ src/main/java/io/github/seonrizee/kiosk/required/lv1/Main.java
@@ -10,17 +10,25 @@ public class Main {
             boolean isRunning = true;
             while (isRunning) {
 
+                System.out.println();
+                displayLine();
                 displayInfo("롯데리아");
-                displayInfo("메뉴를 선택해주세요.");
                 displayInfo("0. 종료");
+                displayLine();
+                displayInput("원하는 메뉴의 번호를 입력해주세요.: ");
 
-                int cmd = Integer.parseInt(sc.nextLine());
-                if (cmd == 0) {
-                    displayInfo("키오스크를 종료합니다.");
-                    isRunning = false;
-                } else {
-                    operateMenu(cmd);
+                try {
+                    int cmd = Integer.parseInt(sc.nextLine());
+                    if (cmd == 0) {
+                        displayInfo("키오스크를 종료합니다.");
+                        isRunning = false;
+                    } else {
+                        operateMenu(cmd);
+                    }
+                } catch (NumberFormatException e) {
+                    displayError("숫자 형식으로 입력해주세요. ");
                 }
+
             }
 
         }
@@ -31,8 +39,19 @@ public class Main {
         displayInfo(cmd + "번 메뉴를 선택했습니다.");
     }
 
-    public static void displayInfo(String str) {
-        System.out.println("KIOSK:::: " + str);
+    public static void displayInfo(String msg) {
+        System.out.println("KIOSK:::: " + msg);
     }
 
+    public static void displayError(String msg) {
+        System.out.println("KIOSK_ERROR:::: " + msg);
+    }
+
+    public static void displayInput(String msg) {
+        System.out.print("KIOSK:::: " + msg);
+    }
+
+    public static void displayLine() {
+        System.out.println("KIOSK:::: ====================================");
+    }
 }
