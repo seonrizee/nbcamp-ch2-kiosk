280711ca77ed104687cc46dce6105df68c5e78e8
seonrizee <seonrizee@gmail.com>
2025-07-17 14:08:56 +0900

    feat(lv4): Menu 클래스 구현

---
+++ src/main/java/io/github/seonrizee/kiosk/required/lv4/Menu.java
@@ -0,0 +1,22 @@
+package io.github.seonrizee.kiosk.required.lv4;
+
+import java.util.List;
+
+public class Menu {
+
+    private final String category;
+    private final List<MenuItem> menuItems;
+
+    public Menu(String category, List<MenuItem> menuItems) {
+        this.category = category;
+        this.menuItems = menuItems;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+}
