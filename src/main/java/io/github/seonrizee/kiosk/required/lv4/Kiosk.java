83a764c01dcc8bf0ffe3f2c8ebd90e837f690a1a
seonrizee <seonrizee@gmail.com>
2025-07-17 20:04:46 +0900

    refactor(lv4): print~ 메소드와 display~ 메소드 구분을 위한 메소드명 정리

--- src/main/java/io/github/seonrizee/kiosk/required/lv4/Kiosk.java
+++ src/main/java/io/github/seonrizee/kiosk/required/lv4/Kiosk.java
@@ -17,18 +17,18 @@ public class Kiosk {
             while (true) {
 
                 System.out.println();
-                displayLine();
-                displayInfo("Welcome to Five Guys");
-                displayTitle("MAIN");
+                printLine();
+                printInfo("Welcome to Five Guys");
+                printTitle("MAIN");
                 displayMenuList(menuList);
 
-                displayInfo("0. 종료");
-                displayLine();
-                displayInput("원하는 카테고리 또는 기능의 번호를 입력해주세요.: ");
+                printInfo("0. 종료");
+                printLine();
+                printInput("원하는 카테고리 또는 기능의 번호를 입력해주세요.: ");
 
                 int selectedMenuIdx = getUserInput(sc, menuList.size());
                 if (selectedMenuIdx == 0) {
-                    displayInfo("키오스크를 종료합니다.");
+                    printInfo("키오스크를 종료합니다.");
                     break;
                 } else {
                     showSubMenu(sc, selectedMenuIdx);
@@ -48,9 +48,9 @@ public class Kiosk {
                 }
                 return input;
             } catch (NumberFormatException e) {
-                displayError("숫자 형식으로 입력해주세요.");
+                printError("숫자 형식으로 입력해주세요.");
             } catch (IndexOutOfBoundsException e) {
-                displayError("잘못된 번호를 입력하셨습니다. 키오스크의 번호를 입력해주세요.");
+                printError("잘못된 번호를 입력하셨습니다. 키오스크의 번호를 입력해주세요.");
             }
         }
     }
@@ -62,17 +62,17 @@ public class Kiosk {
 
         while (true) {
             System.out.println();
-            displayLine();
-            displayTitle(selectedMenu.getCategory());
+            printLine();
+            printTitle(selectedMenu.getCategory());
             displayMenuItems(menuItemList);
 
-            displayInfo("0. 뒤로 가기");
-            displayLine();
-            displayInput("원하는 메뉴의 번호를 입력해주세요.: ");
+            printInfo("0. 뒤로 가기");
+            printLine();
+            printInput("원하는 메뉴의 번호를 입력해주세요.: ");
 
             int selectedItemIdx = getUserInput(sc, menuItemList.size());
             if (selectedItemIdx == 0) {
-                displayInfo("이전 화면으로 돌아갑니다.");
+                printInfo("이전 화면으로 돌아갑니다.");
                 break;
             } else {
                 MenuItem selectedItem = menuItemList.get(selectedItemIdx - 1);
@@ -82,12 +82,12 @@ public class Kiosk {
     }
 
     public void processOrder(int menuItemIdx, MenuItem menuItem) {
-        displayInfo(menuItemIdx + "번 " + menuItem.getName() + "를 주문하셨습니다. 결제 완료됐습니다. 맛있게 드세요!");
+        printInfo(menuItemIdx + "번 " + menuItem.getName() + "를 주문하셨습니다. 결제 완료됐습니다. 맛있게 드세요!");
     }
 
     public void displayMenuList(List<Menu> menuList) {
         for (int idx = 0; idx < menuList.size(); idx++) {
-            displayInfo(idx + 1 + ". " + menuList.get(idx).getCategory());
+            printInfo(idx + 1 + ". " + menuList.get(idx).getCategory());
         }
     }
 
@@ -102,23 +102,23 @@ public class Kiosk {
         System.out.printf(format, idx, menuItem.getName(), menuItem.getPrice(), menuItem.getDesc());
     }
 
-    public void displayTitle(String msg) {
+    public void printTitle(String msg) {
         System.out.println("KIOSK:::: [ " + msg + " MENU ] ");
     }
 
-    public void displayInfo(String msg) {
+    public void printInfo(String msg) {
         System.out.println("KIOSK:::: " + msg);
     }
 
-    public void displayError(String msg) {
+    public void printError(String msg) {
         System.out.print("KIOSK_ERROR:::: " + msg + ":");
     }
 
-    public void displayInput(String msg) {
+    public void printInput(String msg) {
         System.out.print("KIOSK:::: " + msg);
     }
 
-    public void displayLine() {
+    public void printLine() {
         System.out.println("KIOSK:::: ====================================");
     }
 }
