0dfd8c74d9bea49482b4149cfda32bcc9b383f20
seonrizee <seonrizee@gmail.com>
2025-07-17 11:16:15 +0900

    refactor(lv2): MenuItem 객체와 리스트를 사용하도록 리팩터링

--- src/main/java/io/github/seonrizee/kiosk/required/lv2/Main.java
+++ src/main/java/io/github/seonrizee/kiosk/required/lv2/Main.java
@@ -1,18 +1,18 @@
 package io.github.seonrizee.kiosk.required.lv2;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // 임시 메뉴아이템 배열
-        String[] menuItemNames = {"HAMBURGER", "CHEESEBURGER", "BACON BURGER"};
-        int[] menuItemPrices = {13400, 14900, 15900};
-        String[] menuItemDesc = {
-                "부드러운 참깨 번 사이에 갓 구운 신선한 수제 패티 2장을 얹어 제공합니다.",
-                "부드러운 참깨 번 사이에 아메리칸 스타일 치즈를 녹여 넣은 수제 패티 2장을 얹어 제공합니다.",
-                "부드러운 참깨 번 사이에 바삭한 애플우드 베이컨과 수제 패티 2장을 얹어 제공합니다."};
+        // 메뉴아이템 배열 생성
+        List<MenuItem> menuItems = List.of(
+                new MenuItem("HAMBURGER", 13400, "부드러운 참깨 번 사이에 갓 구운 신선한 수제 패티 2장을 얹어 제공합니다."),
+                new MenuItem("CHEESEBURGER", 14900, "부드러운 참깨 번 사이에 아메리칸 스타일 치즈를 녹여 넣은 수제 패티 2장을 얹어 제공합니다."),
+                new MenuItem("BACON BURGER", 15900, "부드러운 참깨 번 사이에 바삭한 애플우드 베이컨과 수제 패티 2장을 얹어 제공합니다.")
+        );
 
         try (Scanner sc = new Scanner(System.in)) {
             boolean isRunning = true;
@@ -21,8 +21,8 @@ public class Main {
                 System.out.println();
                 displayLine();
                 displayInfo("Welcome to Five Guys");
-                for (int idx = 0; idx < menuItemNames.length; idx++) {
-                    displayMenuItem(idx + 1, menuItemNames[idx], menuItemPrices[idx], menuItemDesc[idx]);
+                for (int idx = 0; idx < menuItems.size(); idx++) {
+                    displayMenuItem(idx + 1, menuItems.get(idx));
                 }
                 displayInfo("0. 종료");
                 displayLine();
@@ -34,12 +34,13 @@ public class Main {
                         displayInfo("키오스크를 종료합니다.");
                         isRunning = false;
                     } else {
-                        processOrder(cmd, menuItemNames[cmd - 1]);
+                        MenuItem selectedItem = menuItems.get(cmd - 1);
+                        processOrder(cmd, selectedItem);
                     }
                 } catch (NumberFormatException e) {
                     displayError("숫자 형식으로 입력해주세요. ");
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    displayError("잘못된 번호를 입력하셨습니다. 메뉴판의 번호를 입력해주세요.");
+                } catch (IndexOutOfBoundsException e) {
+                    displayError("잘못된 번호를 입력하셨습니다. 키오스크의 번호를 입력해주세요.");
                 }
 
             }
@@ -48,17 +49,17 @@ public class Main {
 
     }
 
-    public static void processOrder(int cmd, String name) {
-        displayInfo(cmd + "번 " + name + "를 주문하셨습니다. 결제 완료됐습니다. 맛있게 드세요!");
+    public static void processOrder(int cmd, MenuItem item) {
+        displayInfo(cmd + "번 " + item.getName() + "를 주문하셨습니다. 결제 완료됐습니다. 맛있게 드세요!");
     }
 
     public static void displayInfo(String msg) {
         System.out.println("KIOSK:::: " + msg);
     }
 
-    public static void displayMenuItem(int idx, String name, int price, String description) {
+    public static void displayMenuItem(int idx, MenuItem menuItem) {
         String format = "KIOSK:::: %d. %-12s | %,6d원 | %s\n";
-        System.out.printf(format, idx, name, price, description);
+        System.out.printf(format, idx, menuItem.getName(), menuItem.getPrice(), menuItem.getDesc());
     }
 
     public static void displayError(String msg) {
