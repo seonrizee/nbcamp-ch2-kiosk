1f98644347a3bea72c72b0fd81002ba91c46badb
seonrizee <seonrizee@gmail.com>
2025-07-17 11:02:26 +0900

    feat(lv2): 개별 음식 항목을 나타내는 MenuItem 클래스 구현

---
+++ src/main/java/io/github/seonrizee/kiosk/required/lv2/MenuItem.java
@@ -0,0 +1,38 @@
+package io.github.seonrizee.kiosk.required.lv2;
+
+public class MenuItem {
+
+    private String name;
+    private int price;
+    private String desc;
+
+    public MenuItem(String name, int price, String desc) {
+        this.name = name;
+        this.price = price;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
