4a09c618f4e804c6295e26c203ab895e23cac624
seonrizee <seonrizee@gmail.com>
2025-07-21 11:09:40 +0900

    chore(ch-lv2): challenge lv2 개발을 위한 코드 복사

---
+++ src/main/java/io/github/seonrizee/kiosk/challenge/lv2/CartItem.java
@@ -0,0 +1,32 @@
+package io.github.seonrizee.kiosk.challenge.lv2;
+
+public class CartItem {
+
+    private final MenuItem item;
+    private int quantity;
+
+    public CartItem(MenuItem item) {
+        this.item = item;
+        this.quantity = 1;
+    }
+
+    public void increaseQuantity() {
+        this.quantity += 1;
+    }
+
+    public void decreaseQuantity() {
+        this.quantity -= 1;
+    }
+
+    public MenuItem getItem() {
+        return item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getSumItemPrice() {
+        return item.getPrice() * quantity;
+    }
+}
---
+++ src/main/java/io/github/seonrizee/kiosk/challenge/lv2/Menu.java
@@ -0,0 +1,22 @@
+package io.github.seonrizee.kiosk.challenge.lv2;
+
+import java.util.List;
+
+public class Menu {
+
+    private final String category;
+    private final List<MenuItem> menuItems;
+
+    public Menu(String category, List<MenuItem> menuItems) {
+        this.category = category;
+        this.menuItems = menuItems;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return List.copyOf(menuItems);
+    }
+}
